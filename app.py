2defc8d61c8a1032bd066d4ae0c15846a7e7ae1b
viniciusrnascimento <vinicius.rnascimento1@gmail.com>
2025-11-17 21:45:28 -0300

    Criação do login e senha

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request,redirect
 from models import Usuario
 from db import db
 
@@ -6,6 +6,22 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
 
 db.init_app(app)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/registrar', methods=['GET', 'POST'])
+def registrar():
+    if request.method == 'GET':
+        return render_template('registrar.html')
+    elif request.method == 'POST':
+        nome = request.form['nomeForm']
+        senha = request.form['senhaForm']
+
+        novo_usuario = Usuario(nome=nome, senha=senha)
+        db.session.add(novo_usuario)
+        db.session.commit()
 
 if __name__ == '__main__':
     with app.app_context():
