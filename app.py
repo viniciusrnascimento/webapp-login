ff3d5a05187d8c9743a9b526fe67b46922ccd52c
viniciusrnascimento <vinicius.rnascimento1@gmail.com>
2025-11-17 21:18:55 -0300

    Configurações Iniciais

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-s
+from flask import Flask
+from models import Usuario
+from db import db
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
+
+db.init_app(app)
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
