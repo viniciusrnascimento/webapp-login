a61c3edb3d06d62a5ea75011ab8418a42a36702a
viniciusrnascimento <vinicius.rnascimento1@gmail.com>
2025-11-21 13:29:00 -0300

    update

--- app.py
+++ app.py
@@ -63,7 +63,7 @@
         return redirect(url_for('login'))
 
 
-    if __name__ == '__main__':
-        with app.app_context():
-            db.create_all()
-            app.run(debug=True)
+    if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=False)
