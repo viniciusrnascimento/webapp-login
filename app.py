3811a1551915e2d72fa58d001931c0bb4db57c31
viniciusrnascimento <vinicius.rnascimento1@gmail.com>
2025-11-21 14:29:51 -0300

    Correção código

--- app.py
+++ app.py
@@ -1,30 +1,30 @@
-    from flask import Flask, render_template, request, redirect, url_for
-    from flask_login import LoginManager, login_user, login_required, logout_user, current_user
-    from models import Usuario
-    from db import db
-    from config import Config
+from flask import Flask, render_template, request, redirect, url_for
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from models import Usuario
+from db import db
+from config import Config
 
-    app = Flask(__name__)
-    app.config.from_object(Config)
+app = Flask(__name__)
+app.config.from_object(Config)
 
-    lm = LoginManager(app)
-    lm.login_view = 'login'
+lm = LoginManager(app)
+lm.login_view = 'login'
 
-    db.init_app(app)
+db.init_app(app)
 
-    @lm.user_loader
-    def user_loader(id):
-        return db.session.query(Usuario).filter_by(id=id).first()
-
-
-    @app.route('/')
-    @login_required
-    def home():
-        return render_template('home.html')
+@lm.user_loader
+def user_loader(id):
+    return db.session.query(Usuario).filter_by(id=id).first()
 
 
-    @app.route('/login', methods=['GET', 'POST'])
-    def login():
+@app.route('/')
+@login_required
+def home():
+    return render_template('home.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
         if request.method == 'GET':
             return render_template('login.html')
 
@@ -40,8 +40,8 @@
         return redirect(url_for('home'))
 
 
-    @app.route('/registrar', methods=['GET', 'POST'])
-    def registrar():
+@app.route('/registrar', methods=['GET', 'POST'])
+def registrar():
         if request.method == 'GET':
             return render_template('registrar.html')
 
@@ -56,13 +56,13 @@
         return redirect(url_for('home'))
 
 
-    @app.route('/logout')
-    @login_required
-    def logout():
+@app.route('/logout')
+@login_required
+def logout():
         logout_user()
         return redirect(url_for('login'))
 
 
-    if __name__ == "__main__":
+if __name__ == "__main__":
     with app.app_context():
         db.create_all()
