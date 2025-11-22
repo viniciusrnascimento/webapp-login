2022579beaaf33dbb6815c07ec5089d90e9cd572
viniciusrnascimento <vinicius.rnascimento1@gmail.com>
2025-11-21 23:39:16 -0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
 from app import app, db
 
+# Expor o app como 'application' (padrão compatível com Azure/Gunicorn)
+application = app
+
 with app.app_context():
     db.create_all()
-
-if __name__ == "__main__":
-    app.run()
