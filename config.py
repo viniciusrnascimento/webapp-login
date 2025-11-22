dd75f3ab7165e019d7ffe56ff2102cbf99b8a4f4
viniciusrnascimento <vinicius.rnascimento1@gmail.com>
2025-11-21 23:50:13 -0300

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv(
         "DATABASE_URL",
-        mssql+pyodbc://adminsql:Admin!123@svr-lab-app.database.windows.net:1433/db-lab-app?driver=ODBC+Driver+18+for+SQL+Server
+        "mssql+pyodbc://adminsql:Admin!123@svr-lab-app.database.windows.net:1433/db-lab-app?driver=ODBC+Driver+18+for+SQL+Server"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv("SECRET_KEY", "chave-super-secreta")
